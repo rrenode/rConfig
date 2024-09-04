57e5d31c1e12a96653790cf2d2900e67a2a79cb0
Robert J Renode IV <robert.renodej@gmail.com>
2024-09-04 15:38:53 -0400

    Enum support!
    I present enum support with rConfig.
    
    Ussage:
    ```python
    from rConfig import config, rEnum
    from enum import Enum
    
    class Color(Enum):
        RED = 1
        GREEN = 2
        BLUE = 3
    
    @config
    class SomeConfig:
        MY_ENUM: rEnum[Color] = Color.RED
    ```

--- rConfig/CustomTypes.py
+++ rConfig/CustomTypes.py
@@ -1,4 +1,25 @@
+from typing import Any, Type
+
 import re
+import enum
+
+class rEnum:
+    def __init__(self, enum_type: Type[enum.Enum]):
+        self.enum_type = enum_type
+
+    def __call__(self, value: Any) -> enum.Enum:
+        if isinstance(value, self.enum_type):
+            return value
+        if isinstance(value, str):
+            try:
+                return self.enum_type[value]
+            except KeyError:
+                raise ValueError(f"Invalid value '{value}' for enum {self.enum_type.__name__}")
+        raise TypeError(f"Cannot convert {value} to {self.enum_type.__name__}")
+
+    @classmethod
+    def __class_getitem__(cls, item: Type[enum.Enum]):
+        return cls(item)
 
 class Address:
     def __init__(self, value: str):
